60b97bbca9842e8689d5712e3f743bf34e6d261a
Papeya-taahaa <Taha7665>
2021-04-07 23:30:50 +0600

    new

--- udemy.js
+++ udemy.js
@@ -1,5 +1,12 @@
 // var myName = "Taha";
 // console.log(myName);
+  
 
-var myName = "Taha";
-console.log(myName);
+
+
+console.log("helle js!");
+
+//  DOM- document object model
+//  selecting method
+// 1-getElementById , 2-getElementByTagName , 3-getElementByClassName
+
